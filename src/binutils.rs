1fe44f7323573b4a6e6c116dae01361350c6cde7
xushanpu123 <50659045+xushanpu123@users.noreply.github.com>
2024-12-11 17:19:49 +0800

    Update binutils.rs

--- src/binutils.rs
+++ src/binutils.rs
@@ -18,7 +18,7 @@ impl BinUtil {
             })
             .any(|line| OsStr::new(line) == which);
         if !check {
-            Cargo::install().arg("cargo-binutils").arg("--locked").invoke();
+            Cargo::install().arg("cargo-binutils").arg("--version").arg("3.0.4").invoke();
         }
         Self(Command::new(which))
     }
